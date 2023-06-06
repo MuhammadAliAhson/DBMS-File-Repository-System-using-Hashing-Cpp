2b3bac8f25f122099cf3a6d564e6440cbfa95b66
Muhammad Ali Ahson <105967134+MuhammadAliAhson@users.noreply.github.com>
2023-06-07 01:45:42 +0500

    Update Support.h

--- Support.h
+++ Support.h
@@ -1,14 +1,11 @@
 #pragma once
-#include "Black.h"
-
-RedBlackTree tree;
-
-
+Avl tree;
+int table = 0;
 void fun2(vector<string> v);
 void clause();
 //---------------============-------------------------//
 void myfun(string str) {
-
+	//cout << concatenate;
 	std::stringstream sstr(str);
 	std::vector<std::string> v;
 	while (sstr.good())
@@ -17,7 +14,6 @@ void myfun(string str) {
 		getline(sstr, substr, ',');
 		v.push_back(substr);
 	}
-
 
 	fun2(v);
 
@@ -33,7 +29,7 @@ void fun2(vector<string> v) {
 	if (v.size() == 8) {
 		a[0] = v[0];
 		a[1] = v[1];
-		a[2] = v[2] + v[3];
+		a[2] = v[2] +","+ v[3];
 		a[3] = v[4];
 		a[4] = v[5];
 		a[5] = v[6];
@@ -42,7 +38,7 @@ void fun2(vector<string> v) {
 	if (v.size() == 9) {
 		a[0] = v[0];
 		a[1] = v[1];
-		a[2] = v[2] + v[3] + v[4];
+		a[2] = v[2] + "," + v[3]+"," + v[4];
 		a[3] = v[5];
 		a[4] = v[6];
 		a[5] = v[7];
@@ -51,7 +47,7 @@ void fun2(vector<string> v) {
 	else if (v.size() == 10) {
 		a[0] = v[0];
 		a[1] = v[1];
-		a[2] = v[2] + v[3] + v[4] + v[5];
+		a[2] = v[2] +"," + v[3]+ "," + v[4]+ "," + v[5];
 		a[3] = v[6];
 		a[4] = v[7];
 		a[5] = v[8];
@@ -75,7 +71,7 @@ void fun2(vector<string> v) {
 	state = a[4];
 	deaths = stoi(a[5]);
 	age_adjust = stof(a[6]);
-	Node* into = new Node;
+	node* into = new node;
 	into->id = id;
 	into->year = year;
 	into->cause_113 = cause_113;
@@ -83,28 +79,29 @@ void fun2(vector<string> v) {
 	into->state = state;
 	into->deaths = deaths;
 	into->age_adjust = age_adjust;
+	//cout << cause_name << endl;
 	if (prince == 1) {//ID
-		tree.insert( into);
-		return;
+		tree.root = tree.insert_Id(tree.root, into);
+
 	}
 	else if (prince == 2) {// year
-		 tree.insert_year( into);
+		tree.root = tree.insert_year(tree.root, into);
 	}
 	else if (prince == 3) {// deaths
-		tree.insert_death(into);
+		tree.root = tree.insert_deaths(tree.root, into);
 	}
 	else if (prince == 4) {// age-dactor
 
-	tree.insert_age(into);
+		tree.root = tree.insert_age_adjust(tree.root, into);
 	}
 	else if (prince == 5) {// state
-		tree.insert_state( into);
+		tree.root = tree.insert_state(tree.root, into);
 	}
 	else if (prince == 6) {//cause name
-		tree.insert_cause_name( into);
+		tree.root = tree.insert_cause_name(tree.root, into);
 	}
-	else if (prince == 7) {
-		tree.insert_cause_113( into);
+	else if (prince == 7) {//cause 113
+		tree.root = tree.insert_cause_113(tree.root, into);
 
 	}
 
@@ -134,6 +131,17 @@ int menu() {
 	string line;
 	ofstream outFile("note_new.txt");
 	int h = 1;
+	/*
+	while (getline(inFile, line)) {
+		outFile << line << endl;
+		if (h >= 2) {
+			myfun(line);
+		}
+		h = 2;
+	}*/
+
+	//inFile.close();
+	/*outFile.close();*/
 	if (table == 0) {
 		for (int i = 0; i < 10; i++) {
 			fstream inFile1(index[i]);
@@ -150,7 +158,7 @@ int menu() {
 		outFile.close();
 	}
 	else {
-		fstream inFile1(index[table - 1]);
+		fstream inFile1(index[table-1]);
 		h = 1;
 		while (getline(inFile1, line)) {
 			if (h >= 2) {
@@ -162,13 +170,14 @@ int menu() {
 
 
 	}
+
 	return 0;
 }
 void P_Search() {
 
-	cout << "Data base mangemant system" << endl;
-	cout << endl;
+	cout << "Data Base Mangement System" << endl;
 t:
+	cout << endl;
 	cout << "Press 1 to sort by ID" << endl;
 	cout << "Press 2 to sort by Year" << endl;
 	cout << "Press 3 to sort by Deaths" << endl;
@@ -186,7 +195,6 @@ t:
 		clause();
 		return;
 	}
-
 	int key = 0, search = 0;
 	cout << "LOADING  .";
 	for (int i = 0; i < 2; i++) {
@@ -208,7 +216,7 @@ t:
 		if (search == 1) {
 			cout << "Enter key to search" << endl;
 			cin >> know;
-			tree.search_id( know);
+			tree.search_id(tree.root, know);
 		}
 		else if (search == 2) {
 			int i = 0, j = 0, range_1 = 0, range_2 = 0;
@@ -219,13 +227,13 @@ t:
 			cin >> range_2;
 			j = range_2;
 			for (i; i < j; i++) {
-				tree.search_id( i);
+				tree.search_id(tree.root, i);
 			}
 
 		}
 		else if (search == 3) {
 			update_id = true;
-			tree.update();
+			tree.update(tree.root);
 			update_id = false;
 		}
 		else if (search == 4) {
@@ -234,9 +242,9 @@ t:
 			tree.delete_id(tree.root, know);
 			goto id;
 		}
-		/*else if (search == 5) {
+		else if (search == 5) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 6) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -246,21 +254,24 @@ t:
 		else {
 			goto id;
 		}
-}
-	//=========================================================================================//		
+
+
+
+		//=========================================================================================//		
+	}
 	if (prince == 2) {
 	year:
 		cout << "Press 1 for the Point Search " << endl;
 		cout << "Press 2 for Range Search" << endl;
 		cout << "Press 3 Update the Node by Year" << endl;
 		cout << "Press 4 to Delete the Node by Year" << endl;
-		/*cout << "Press 5 to Terminate the Program" << endl;*/
+		cout << "Press 5 to Terminate the Program" << endl;
 		cout << "Press 6 to sort on Different Choice" << endl;
 		cin >> search;
 		if (search == 1) {
 			cout << "Enter Year to search" << endl;
 			cin >> key;
-			tree.search_year( key);
+			tree.search_year(tree.root, key);
 		}
 		else if (search == 2) {
 			int i = 0, j = 0, range_1 = 0, range_2 = 0;
@@ -271,12 +282,12 @@ t:
 			cin >> range_2;
 			j = range_2;
 			for (i; i < j; i++) {
-				tree.search_year( i);
+				tree.search_year(tree.root, i);
 			}
 		}
 		else if (search == 3) {
 			update_year = true;
-			tree.update();
+			tree.update(tree.root);
 			update_year = false;
 		}
 		else if (search == 4) {
@@ -285,9 +296,9 @@ t:
 			tree.delete_year(tree.root, know);
 			goto year;
 		}
-		/*else if (search == 5) {
+		else if (search == 5) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 6) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -297,21 +308,28 @@ t:
 		else {
 			goto year;
 		}
+
+
+
+
+		//==============================================================================//
+
 	}
-	//==============================================================================//
+
+
 	if (prince == 3) {
 	death:
 		cout << "Press 1 for the Point Search " << endl;
 		cout << "Press 2 for Range Search" << endl;
 		cout << "Press 3 Update the Node by Death" << endl;
 		cout << "Press 4 to Delete the Node by Death" << endl;
-		/*cout << "Press 5 to Terminate the Program" << endl;*/
+		cout << "Press 5 to Terminate the Program" << endl;
 		cout << "Press 6 to sort on Different Choice" << endl;
 		cin >> search;
 		if (search == 1) {
 			cout << "Enter death to search" << endl;
 			cin >> key;
-			tree.search_deaths( key);
+			tree.search_death(tree.root, key);
 		}
 		else if (search == 2) {
 			int i = 0, j = 0, range_1 = 0, range_2 = 0;
@@ -322,23 +340,23 @@ t:
 			cin >> range_2;
 			j = range_2;
 			for (i; i < j; i++) {
-				tree.search_deaths(i);
+				tree.search_death(tree.root, i);
 			}
 		}
 		else if (search == 3) {
 			update_death = true;
-			tree.update();
+			tree.update(tree.root);
 			update_death = false;
 		}
 		else if (search == 4) {
 			cout << "Enter key to Delete" << endl;
 			cin >> know;
-			tree.delete_deaths(tree.root, know);
+			tree.delete_death(tree.root, know);
 			goto death;
 		}
-		/*else if (search == 5) {
+		else if (search == 5) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 6) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -348,23 +366,24 @@ t:
 		else {
 			goto death;
 		}
+
+
+
+		//===============================================================================//
 	}
-
-	//===============================================================================//
-
 	if (prince == 4) {
 	age:
 		cout << "Press 1 for the Point Search " << endl;
 		cout << "Press 2 for Range Search" << endl;
 		cout << "Press 3 Update the Node by Age" << endl;
 		cout << "Press 4 to Delete the Node by Age" << endl;
-		/*cout << "Press 5 to Terminate the Program" << endl;*/
+		cout << "Press 5 to Terminate the Program" << endl;
 		cout << "Press 6 to sort on Different Choice" << endl;
 		cin >> search;
 		if (search == 1) {
 			cout << "Enter key to search" << endl;
 			cin >> ratio;
-			tree.search_age( ratio);
+			tree.search_age(tree.root, ratio);
 		}
 		else if (search == 2) {
 			float i = 0, j = 0, range_1 = 0, range_2 = 0;
@@ -375,12 +394,12 @@ t:
 			cin >> range_2;
 			j = range_2;
 			for (i; i < j; i = i + 0.1) {
-				tree.search_age( i);
+				tree.search_age(tree.root, i);
 			}
 		}
 		else if (search == 3) {
 			update_age = true;
-			tree.update();
+			tree.update(tree.root);
 			update_age = false;
 		}
 		else if (search == 4) {
@@ -390,9 +409,9 @@ t:
 			tree.delete_age(tree.root, fp);
 			goto age;
 		}
-		/*else if (search == 5) {
+		else if (search == 5) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 6) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -402,24 +421,26 @@ t:
 		else {
 			goto age;
 		}
+
+
+		//=================================================================//
 	}
-	//=================================================================//
 	if (prince == 5) {
 	state:
 		cout << "Enter 1 for state to search" << endl;
 		cout << "Press 2 Update the Node by state" << endl;
 		cout << "Press 3 to Delete the Node by state" << endl;
-		/*cout << "Press 4 to Terminate the Program" << endl;*/
+		cout << "Press 4 to Terminate the Program" << endl;
 		cout << "Press 5 to sort on Different Choice" << endl;
 		cin >> search;
 		if (search == 1) {
 			cout << "Enter state to search" << endl;
 			cin >> giga;
-			tree.search_state( giga);
+			tree.search_state(tree.root, giga);
 		}
 		else if (search == 2) {
 			update_state = true;
-			tree.update();
+			tree.update(tree.root);
 			update_state = false;
 		}
 		else if (search == 3) {
@@ -429,9 +450,9 @@ t:
 			tree.delete_state(tree.root, y);
 			goto state;
 		}
-		/*else if (search == 4) {
+		else if (search == 4) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 5) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -441,8 +462,8 @@ t:
 		else {
 			goto state;
 		}
+		//===============================================================================//
 	}
-	//===============================================================================//
 	if (prince == 6) {
 		//Unintentional injuries
 		//Heart disease
@@ -453,18 +474,18 @@ t:
 		cout << "Enter 1 to cause name to search" << endl;
 		cout << "Enter 2 Update the Node by Cause name" << endl;
 		cout << "Press 3 to Delete the Node by Cause Name" << endl;
-	/*	cout << "Press 4 to Terminate the Program" << endl;*/
+		cout << "Press 4 to Terminate the Program" << endl;
 		cout << "Press 5 to sort on Different Choice" << endl;
 		cin >> search;
 
 		if (search == 1) {
 			cout << "Enter cause name to search" << endl;
 			cin >> giga;
-			tree.search_cause_name( giga);
+			tree.search_cause_name(tree.root, giga);
 		}
 		else if (search == 2) {
 			update_cause_name = true;
-			tree.update();
+			tree.update(tree.root);
 			update_cause_name = false;
 		}
 		else if (search == 3) {
@@ -473,9 +494,9 @@ t:
 			tree.delete_cause_name(tree.root, giga);
 			goto cause_name;
 		}
-	/*	else if (search == 4) {
+		else if (search == 4) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 5) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -486,24 +507,23 @@ t:
 			goto cause_name;
 		}
 	}
-	//===============================================================================//
 	if (prince == 7) {
 	cause_113:
 		giga = "\0";
 		cout << "Enter 1 for cause 113 to search" << endl;
 		cout << "Enter 2 Update the Node by Cause 113" << endl;
 		cout << "Press 3 to Delete the Node by Cause 113" << endl;
-	/*	cout << "Press 4 to Terminate the Program" << endl;*/
+		cout << "Press 4 to Terminate the Program" << endl;
 		cout << "Press 5 to sort on Different Choice" << endl;
 		cin >> search;
 		if (search == 1) {
 			cout << "Enter cause 113 to search" << endl;
 			cin >> giga;
-			tree.search_cause_113( giga);
-		} 
+			tree.search_cause_113(tree.root, giga);
+		}
 		else if (search == 2) {
 			update_cause_113 = true;
-			tree.update();
+			tree.update(tree.root);
 			update_cause_113 = false;
 		}
 		else if (search == 3) {
@@ -512,9 +532,9 @@ t:
 			tree.delete_cause_113(tree.root, giga);
 			goto cause_113;
 		}
-	/*	else if (search == 4) {
+		else if (search == 4) {
 			tree.levelorder_newline();
-		}*/
+		}
 		else if (search == 5) {
 			delete(tree.root->left);
 			delete(tree.root->right);
@@ -526,14 +546,18 @@ t:
 		}
 
 	}
+	else if (prince == 8) {
+		tree.levelorder_newline();
+	}
 	else {
 		PlaySound(TEXT("error.wav"), NULL, SND_SYNC);
 		goto t;
 	}
+
+
 	return;
 
 }
-
 
 //=============================================================================================//
 void clause() {
@@ -554,13 +578,15 @@ void clause() {
 	cout << "You should remember that there are 10 tables. Form 1 - 10" << endl;
 	string a, b;
 
-lol:
+	lol:
 	PlaySound(TEXT("details.wav"), NULL, SND_SYNC);
-	cout << "SELECT THE TABLE NUMBER FROM 1 -  10  " << endl;
+	cout << "SELECT THE TABLE NUMBER FROM 1 -  10  "<<endl ;
 	cout << "OR" << endl;
 	cout << "PRESS 0 TO SELECT ALL THE FILES       ";
 	cin >> table;
-
+	if (table > 10 || table < 0) {
+		goto lol;
+	}
 	cout << "Select the column first" << endl;
 	cout << "     SELECT	:  ";
 	cin >> a;
@@ -568,49 +594,49 @@ lol:
 	cout << "     WHERE	    :  ";
 	cin >> b;
 	cout << endl;
-	cout << "IS EQUAL TO =  :";
+	cout << "   IS EQUAL TO =  :";
 	cin >> a;
 	if (b == "ID" || b == "id" || b == "Id") {
 		prince = 1;
 		menu();
 		int val = stoi(a);
-		tree.search_id( val);
+		tree.search_id(tree.root, val);
 	}
 	else if (b == "State" || b == "STATE" || b == "state") {
 		prince = 5;
 		menu();
-		tree.search_state( a);
+		tree.search_state(tree.root, a);
 	}
 	else if (b == "Cause Name" || b == "cause name") {
 		prince = 6;
 		menu();
-		tree.search_cause_name( a);
+		tree.search_cause_name(tree.root, a);
 	}
 	else if (b == "YEAR" || b == "year" || b == "Year") {
 		prince = 2;
 		menu();
 		int val = stol(a);
-		tree.search_year( val);
+		tree.search_year(tree.root, val);
 	}
 	else if (b == "DEATHS" || b == "deaths" || b == "Deaths") {
 		prince = 3;
 		menu();
 		int val = stol(a);
-		tree.search_deaths( val);
+		tree.search_death(tree.root, val);
 	}
 	else if (b == "CAUSE 113" || b == "Cause 113" || b == "cause 113") {
 		prince = 7;
 		menu();
-		tree.search_cause_113( a);
+		tree.search_cause_113(tree.root, a);
 	}
 	else if (b == "AGE FACTOR" || b == "Age Factor" || b == "age factor") {
 		prince = 4;
 		menu();
 		float val = stol(a);
-		tree.search_age( val);
+		tree.search_age(tree.root, val);
 	}
 	else {
-		cout << "WRONG INPUT";
+		cout << "WRONG INPUT"<<endl;
 		goto lol;
 	}
 	PlaySound(TEXT("end.wav"), NULL, SND_SYNC);
